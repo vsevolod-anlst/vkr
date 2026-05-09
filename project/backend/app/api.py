b9603e92d00100cfe55a60a08932e7a0b5ea5af1
suvsedmi <suvsedmi>
2026-05-09 23:05:50 +0300

    добавил скачивание модели для превращения запроса в вектор, разобрался с рекваерментсами, добавил тестовую ручку проверки что ретривер находит чанки - находит

--- project/backend/app/api.py
+++ project/backend/app/api.py
@@ -5,6 +5,9 @@ from app.database import SessionLocal
 from app.rag.model import QueryLog
 from app.tasks import process_question
 
+from app.rag import retriever
+
+
 app = FastAPI()
 
 @app.post("/ask")
@@ -16,3 +19,9 @@ def ask(question: str):
 def get_result(log_id: int):
     db = SessionLocal()
     return db.query(QueryLog).filter(QueryLog.id == log_id).first()
+
+
+@app.get("/test_retriever")
+def rag_test(query: str, top_k: int = 5):
+    results = retriever.retrieve(query, top_k=top_k)
+    return {"results": results}
