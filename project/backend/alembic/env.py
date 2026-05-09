3f0c37cb4e61c215c5c8f0b7370e4bb5fbf56ab9
suvsedmi <suvsedmi>
2026-05-09 12:29:16 +0300

    сделал миграцию

---
+++ project/backend/alembic/env.py
@@ -0,0 +1,53 @@
+from logging.config import fileConfig
+from sqlalchemy import create_engine, pool
+from alembic import context
+
+import os
+from app.rag.model import Base
+from app.config import POSTGRES_URL
+
+config = context.config
+
+# подставляем URL из .env
+config.set_main_option("sqlalchemy.url", POSTGRES_URL)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    connectable = create_engine(
+        config.get_main_option("sqlalchemy.url"),
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
+
